7c2020fc360256a0d9507829406a7b9b793d8567
Aaron <aaron1@metschnet.com>
2022-03-30 10:38:03 -0400

    commit

--- src/Simplifier/Fraction.py
+++ src/Simplifier/Fraction.py
@@ -7,3 +7,16 @@ class Fraction:
     def __init__(self, num: float64 | int64, den: float64 | int64):
         self.den = den
         self.num = num
+    
+    def setNum(self, num: float64 | int64):
+        self.num = num
+    def setDen(self, den: float64 | int64):
+        self.den = den
+    
+    def getNum(self):
+        return self.num
+    def getDen(self):
+        return self.den
+    
+    def eval(self):
+        return float64(self.num/self.den)
